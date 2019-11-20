2875f7e018c7b589a94a17bd744fb1e1cace3dfe
Alyson <aj16c@my.fsu.edu>
2019-11-20 12:52:56 -0500

    updated the navbar for onclick

--- daddybases.py
+++ daddybases.py
@@ -9,7 +9,7 @@ app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 
-@app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
 
